8450f452780abf5b3e65ac75e27c6488d63d2573
Zhenya Ilchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2023-08-20 22:09:08 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-// alert("Hello from JavaScript");
+import Carousel from "bootstrap/js/dist/carousel";
+import Dropdown from "bootstrap/js/dist/dropdown";
+import Collapse from "bootstrap/js/dist/collapse";
+import searchToggler from "./search-togler";
+
+searchToggler();
