e3044d0e36ab1440f40e0f26e4eca4c225c010db
Zhenya Ilchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2023-08-21 22:01:22 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,34 @@ import Collapse from "bootstrap/js/dist/collapse";
 import searchToggler from "./search-togler";
 
 searchToggler();
+
+import SimpleLightbox from "simplelightbox";
+let lightbox = new SimpleLightbox('.gallery a');
+
+import Masonry from 'masonry-layout';
+
+var msnry = new Masonry( '.masonry', {
+  itemSelector: '.masonry-item',
+  columnWidth: '.masonry-item',
+  percentPosition: true,
+  gutter: '.masonry-gutter',
+  horizontalOrder: true
+});
+
+let masonryGrid = document.querySelector('#masonry');
+let masonryMore = document.querySelector('#masonry-more');
+let masonryMoreItems = document.querySelectorAll('.masonry-item-more');
+
+let showMasonryMoreItems = () => {
+  masonryMoreItems.forEach((item) => {
+    item.classList.remove('masonry-item-more');
+  });
+  masonryMore.style.display = 'none';
+  masonryGrid.classList.remove('masonry-overlay');
+}
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('#masonry-more')) return;
+  showMasonryMoreItems();
+  msnry.layout();
+})
